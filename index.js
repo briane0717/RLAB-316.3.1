548af502a144879ce27e2825e0b0926cb1466315
Brian <briane0717@gmail.com>
2024-11-07 15:21:52 -0500

    finished part 4

---
+++ index.js
@@ -0,0 +1,82 @@
+const menuLinks = [
+  { text: 'about', href: '/about' },
+  { text: 'catalog', href: '#', subLinks: [
+      { text: 'all', href: '/catalog/all' },
+      { text: 'top selling', href: '/catalog/top' },
+      { text: 'search', href: '/catalog/search' },
+    ]
+  },
+  { text: 'orders', href: '#', subLinks: [
+      { text: 'new', href: '/orders/new' },
+      { text: 'pending', href: '/orders/pending' },
+      { text: 'history', href: '/orders/history' },
+    ]
+  },
+  { text: 'account', href: '#', subLinks: [
+      { text: 'profile', href: '/account/profile' },
+      { text: 'sign out', href: '/account/signout' },
+    ]
+  },
+];
+
+// Style and configure the top menu
+const topMenuEl = document.querySelector("#top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+// Create top menu links
+menuLinks.forEach(link => {
+  const a = document.createElement("a");
+  a.setAttribute("href", link.href);
+  a.textContent = link.text;
+  topMenuEl.append(a);
+});
+
+// Style and configure the main element
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.textContent = document.title;
+mainEl.classList.add("flex-ctr");
+
+// Style and configure the sub-menu
+const subMenuEl = document.querySelector("#sub-menu");
+subMenuEl.style.height = "100%";
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.classList.add("flex-around");
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
+
+const topMenuLinks = topMenuEl.querySelectorAll("a");
+
+// Event listener to handle top menu clicks
+topMenuEl.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  // Ensure the clicked element is an <a> tag
+  if (evt.target.tagName !== "A") return;
+
+  console.log(evt.target.textContent);
+
+  // If the clicked element does not yet have the 'active' class
+  // If it does remove it
+  if (!evt.target.classList.contains('active')) {
+    topMenuLinks.forEach(link => link.classList.remove('active'))
+    evt.target.classList.add('active');
+    // Remove 'active' class from all links
+    ;
+
+    // Find the matching link object in menuLinks
+    let clickedLink = menuLinks.find(link => link.text === evt.target.textContent);
+
+    // Check if clickedLink has subLinks
+    if (clickedLink && clickedLink.subLinks) {
+      subMenuEl.style.top = '100%';
+    } else {
+      subMenuEl.style.top = '0%';
+    }
+  }else{
+    evt.target.classList.remove('active')
+    subMenuEl.style.top = "0%"
+  }
+});
